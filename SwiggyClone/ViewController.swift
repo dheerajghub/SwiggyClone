252b959d9f08306dd6a8560155898f603dfca31f
DHEERAJ <dheeraj@appscrip.co>
2022-01-29 11:34:59 +0530

    Initial Commit

---
+++ SwiggyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiggyClone
+//
+//  Created by Dheeraj Kumar Sharma on 29/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
